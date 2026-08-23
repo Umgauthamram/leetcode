0b37e8d9faf93a74874120ee7e6c275b77fd43d9
Gautham Ram <gauthamram.um@gmail.com>
2026-08-23 17:20:10 +0530

    10651 The smallest Topological Sort

--- 10651 The smallest Topological Sort/code.py
+++ 10651 The smallest Topological Sort/code.py
@@ -1,7 +1,6 @@
 from collections import defaultdict, deque
 import sys
 
-
 def eventualSafeNodes():
   input_data = sys.stdin.read().split()
   if not input_data:
@@ -9,28 +8,28 @@ def eventualSafeNodes():
 
   n = int(input_data[0])
 
-  idx = 1
-  reversed_adj = defaultdict(list)
-  in_degree = [0] * n
+  x = 1
+  reversedadj = defaultdict(list)
+  indegree = [0] * n
 
   for u in range(n):
     for v in range(n):
-      val = int(input_data[idx])
-      idx += 1
+      val = int(input_data[x])
+      x += 1
       if val == 1:
-        reversed_adj[v].append(u)
-        in_degree[u] += 1
+        reversedadj[v].append(u)
+        indegree[u] += 1
 
-  queue = deque([i for i in range(n) if in_degree[i] == 0])
+  queue = deque([i for i in range(n) if indegree[i] == 0])
   safe_nodes = []
   
   while queue:
     node = queue.popleft()
     safe_nodes.append(node)
 
-    for neighbor in reversed_adj[node]:
-      in_degree[neighbor] -= 1
-      if in_degree[neighbor] == 0:
+    for neighbor in reversedadj[node]:
+      indegree[neighbor] -= 1
+      if indegree[neighbor] == 0:
         queue.append(neighbor)
 
   safe_nodes.sort()
