35281b594dfbfb9b57525fc4338572dfbf055641
Gautham Ram <gauthamram.um@gmail.com>
2026-09-02 11:53:41 +0530

    Beautiful Arrangement

---
+++ dojo/6th belt/Beautiful Arrangement/code.py
@@ -0,0 +1,30 @@
+# LeetCode 526
+
+# input
+# 2
+
+# output
+# 2
+
+def countArrangement(n):
+    visited = [False] * (n + 1)
+    
+    def backtrack(index):
+        
+        if index>n:
+            return 1
+
+        count = 0
+        for num in range(1,n+1):
+            if not visited[num] and (num % index == 0 or index % num ==0):
+                visited[num] = True
+                count +=backtrack(index+1)
+                visited[num] = False
+
+        return count
+    return backtrack(1)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(countArrangement(n))
